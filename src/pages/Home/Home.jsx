6875a499e4f22d224f0e5342be2cbad9a0375543
creyes25 <carolina.reyes2022@gmail.com>
2022-11-12 13:21:33 -0600

    add my name, and title in home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,14 @@
 function Home() {
   return (
-    <h1>Home</h1>
+    <>
+      <div>
+        <h1>Carolina Reyes</h1>
+        <h2>Software Engineer</h2>
+      </div>
+      <div>
+        <img src="" alt="Myself" />
+      </div>
+    </>
   )
 }
 
