6e02c354e38c1dbc0f3ae43519386ba2dea6b8c8
creyes25 <carolina.reyes2022@gmail.com>
2022-11-12 12:42:46 -0600

    put page components into folders

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,11 @@ import { Routes, Route } from 'react-router-dom'
 import './App.css';
 
 // page components
-import Home from './pages/Home'
-import About from './pages/About'
-import Contact from './pages/Contact'
-import Resume from './pages/Resume'
-import Projects from './pages/Projects'
+import Home from './pages/Home/Home'
+import About from './pages/About/About'
+import Contact from './pages/Contact/Contact'
+import Resume from './pages/Resume/Resume'
+import Projects from './pages/Projects/Projects'
 
 
 function App() {
