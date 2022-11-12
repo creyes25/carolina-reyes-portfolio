e6e320b30e0badefc91aa0b1f59c0a20d98daba3
creyes25 <carolina.reyes2022@gmail.com>
2022-11-12 16:11:46 -0600

    add contact information in contact page

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,6 +1,20 @@
 function Contact() {
   return (
-    <h1>Contact</h1>
+    <main>
+      <div>
+        <h1>Contact</h1>
+        <p>
+          If you'd like collaborate on an interesting project or just chat code, feel free to hit me up via <a href="mailto:carolina.reyes2022@gmail.com">email</a>.
+        </p>
+      </div>
+      <div>
+        <p>Reach out!</p>
+        <ul>
+          <li><a href="https://www.linkedin.com/in/carolinareyes25/">LinkedIn</a></li>
+          <li><a href="https://github.com/creyes25">GitHub</a></li>
+        </ul>
+      </div>
+    </main>
   )
 }
 
