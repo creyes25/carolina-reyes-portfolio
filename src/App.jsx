b82bfe0ad88fb7895658753ef79531c3e44788cc
creyes25 <carolina.reyes2022@gmail.com>
2022-11-12 13:21:06 -0600

    add a navigation bar

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom'
 import './App.css';
 
 // page components
+import NavBar from './components/NavBar/NavBar'
 import Home from './pages/Home/Home'
 import About from './pages/About/About'
 import Contact from './pages/Contact/Contact'
@@ -12,15 +13,19 @@ import Projects from './pages/Projects/Projects'
 
 function App() {
   return (
-    <div>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='about' element={<About />} />
-        <Route path='contact' element={<Contact />} />
-        <Route path='resume' element={<Resume />} />
-        <Route path='projects' element={<Projects />} />
-      </Routes>
-    </div>
+    <>
+      <NavBar />
+      <div>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='about' element={<About />} />
+          <Route path='contact' element={<Contact />} />
+          <Route path='resume' element={<Resume />} />
+          <Route path='projects' element={<Projects />} />
+        </Routes>
+      </div>
+    
+    </>
   );
 }
 
