1017849c7f58f706460d49f3b727b4bb0bc20ab0
creyes25 <carolina.reyes2022@gmail.com>
2022-11-12 16:27:21 -0600

    add my resume and make a dowloadable link

--- src/pages/Resume/Resume.jsx
+++ src/pages/Resume/Resume.jsx
@@ -1,6 +1,13 @@
 function Resume() {
   return (
-    <h1>Resume</h1>
+    <main>
+      <h1>Resume</h1>
+      <div>
+        <p>
+          Download my <a href="/resume/carolina-reyes-resume2.pdf" download>resume</a>.
+        </p>
+      </div>
+    </main>
   )
 }
 
