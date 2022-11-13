8efefadc70014812f7238266f98ada32b03f968f
creyes25 <carolina.reyes2022@gmail.com>
2022-11-13 15:38:16 -0600

    add all projects information

---
+++ src/data/project.js
@@ -0,0 +1,39 @@
+const projects = [
+  {
+    title: "Giftology",
+    description: "This is a MERN stack CRUD application that helps users to share their wishlists with friends to improve gift-giving",
+    image: "",
+    repositoryLink: "https://github.com/creyes25/giftology-frontend",
+    deploymentLink: "https://giftology-madeez.netlify.app/",
+  },
+  {
+    title: "Workout Tracker",
+    description: "A MEN Stack CRUD Application that helps users to track their workouts",
+    image: "",
+    repositoryLink: "https://github.com/creyes25/workout-tracker",
+    deploymentLink: "https://track-workouts.fly.dev/",
+  },
+  {
+    title: "Bird Collector",
+    description: "A simple Django / Python app that help users collect birds and track their feedings.",
+    image: "",
+    repositoryLink: "https://github.com/creyes25/bird-collector",
+    deploymentLink: "",
+  },
+  {
+    title: "Mr.Cuddles",
+    description: "A friendly pet stimulator game where you have to take care of a Panda using Javascript, HTML, and CSS",
+    image: "",
+    repositoryLink: "https://github.com/creyes25/Pet-Stimulator",
+    deploymentLink: "https://mrcuddles.netlify.app/",
+  },
+  {
+    title: "Tik-Tac-Toe",
+    description: "A friendly game of tic tac toe, using Javascript, HTML, and CSS",
+    image: "",
+    repositoryLink: "https://github.com/creyes25/ttt-weekend",
+    deploymentLink: "https://carolina-tic-tac-toe.netlify.app/",
+  },
+]
+
+export default projects
