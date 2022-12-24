0a60b7acd401abb155d0d0486fc0784efe59d6e2
creyes25 <carolina.reyes2022@gmail.com>
2022-12-23 21:41:16 -0600

    add technologies of each project

--- src/data/project.js
+++ src/data/project.js
@@ -1,17 +1,20 @@
 const projects = [
   {
     title: "Giftology",
-    description: "This is a MERN stack CRUD application that helps users to share their wishlists with friends to improve gift-giving",
+    description: "This is a MERN stack CRUD application that helps users to share their wishlists with friends to improve gift-giving.",
     image: "../images/projects/giftology.png",
     repositoryLink: "https://github.com/creyes25/giftology-frontend",
     deploymentLink: "https://giftology-madeez.netlify.app/",
+    technologies : ["HTML", "CSS", "Bootstrap", "React", "JavaScript", "NodeJS", "MongoDB"],
   },
   {
     title: "Workout Tracker",
-    description: "A MEN Stack CRUD Application that helps users to track their workouts",
+    description: "A MEN Stack CRUD Application that helps users to track their workouts.",
     image: "../images/projects/workout-tracker.png",
     repositoryLink: "https://github.com/creyes25/workout-tracker",
     deploymentLink: "https://track-workouts.fly.dev/",
+    technologies : ["CSS", "Bootstrap", "JavaScript", "NodeJS", "MongoDB"],
+
   },
   {
     title: "Bird Collector",
@@ -19,20 +22,26 @@ const projects = [
     image: "../images/projects/bird-collector.png",
     repositoryLink: "https://github.com/creyes25/bird-collector",
     deploymentLink: "https://bird-collector-django.herokuapp.com/",
+    technologies : ["HTML", "CSS", "Django", "Python", "PostgreSQL", "Docker"],
+
   },
   {
     title: "Mr.Cuddles",
-    description: "A friendly pet stimulator game where you have to take care of a Panda using Javascript, HTML, and CSS",
+    description: "A friendly pet stimulator game where you have to take care of a Panda using Javascript, HTML, and CSS.",
     image: "../images/projects/pet-stimulator.png",
     repositoryLink: "https://github.com/creyes25/Pet-Stimulator",
     deploymentLink: "https://mrcuddles.netlify.app/",
+    technologies : ["HTML", "CSS", "Bootstrap", "JavaScript"],
+
   },
   {
     title: "Tik-Tac-Toe",
-    description: "A friendly game of tic tac toe, using Javascript, HTML, and CSS",
+    description: "A friendly game of tic tac toe, using Javascript, HTML, and CSS.",
     image: "../images/projects/tic-tac-toe.png",
     repositoryLink: "https://github.com/creyes25/ttt-weekend",
     deploymentLink: "https://carolina-tic-tac-toe.netlify.app/",
+    technologies : ["HTML", "CSS", "Bootstrap", "JavaScript"],
+
   },
 ]
 
