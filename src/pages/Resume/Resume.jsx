2e364442fd83905302bbcc726079b3ac3fec31e0
creyes25 <carolina.reyes2022@gmail.com>
2022-12-23 22:36:18 -0600

    update resume file

--- src/pages/Resume/Resume.jsx
+++ src/pages/Resume/Resume.jsx
@@ -6,7 +6,7 @@ function Resume() {
       <h1>Resume</h1>
       <p>Click icon to download my resume </p>
       <div className={styles.resume_icon}>
-        <a href="/resume/porfolioResume.pdf" download><img src="../images/resume-icon.png" alt="" /></a>
+        <a href="/resume/portfolioResume.pdf" download><img src="../images/resume-icon.png" alt="" /></a>
       </div>
     </main>
   )
