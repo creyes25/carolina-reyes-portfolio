0394bb2c8d32639b5130db61e14b93485c61a5d1
creyes25 <carolina.reyes2022@gmail.com>
2022-12-24 19:49:25 -0600

    style about description and image

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -3,13 +3,16 @@ function About() {
   return ( 
     <main className={styles.about}>
       <h1 className={styles.title}>About</h1>
-      <div className={styles.about_pic}>
-        <img src="./images/myself.jpg" alt="myself" />
-      </div> 
-      <div className={styles.about_desc}>
-        <p>I’m a Software Engineer that loves to create and transform something out of nothing. I'm driven to learn, persistent, and I'm passionate about finding different ways to problem solve and trying things outside my comfort zone. This allows me to grow not only as a developer but as a person. My dedication and passion allows me to be trustworthy. I'm enthusiastic about working with clients to provide them an outstanding level of support  throughout the process.  
-        </p>
-      </div>   
+      <div className={styles.aboutContainer}>
+        <div className={styles.about_pic}>
+          <img src="./images/myself.jpg" alt="myself" />
+        </div> 
+        <div className={styles.about_desc}>
+          <p>I’m a Software Engineer that loves to create and transform something out of nothing. I'm driven to learn, persistent, and I'm passionate about finding different ways to problem solve and trying things outside my comfort zone. This allows me to grow not only as a developer but as a person. My dedication and passion allows me to be trustworthy. I'm enthusiastic about working with clients to provide them an outstanding level of support  throughout the process.  
+          </p>
+        </div>   
+
+      </div>
     </main>
   )
 }
